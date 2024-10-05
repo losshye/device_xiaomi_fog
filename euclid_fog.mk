a59562ce156cf098304b424adc1d77b8d0daf754
Loosiee <akunfreefire15417@gmail.com>
2024-10-05 19:07:40 +0700

    Update euclid_fog.mk

--- euclid_fog.mk
+++ euclid_fog.mk
@@ -10,6 +10,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common stuff.
 $(call inherit-product, vendor/euclid/config/common_full_phone.mk)
+
+# Inherit from fog device
+$(call inherit-product, device/xiaomi/fog/device.mk)
+
 EUCLID_BUILD_TYPE := UNOFFICIAL
 EUCLID_MAINTAINER := losshye#EW
 TARGET_BOOT_ANIMATION_RES := 720
@@ -17,10 +21,6 @@ EUCLID_GAPPS := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := true
 TARGET_INCLUDE_PIXEL_LAUNCHER := true
 TARGET_SUPPORTS_TOUCHGESTURES := true
-
-# Inherit from fog device
-$(call inherit-product, device/xiaomi/fog/device.mk)
-
 PRODUCT_NAME := euclid_fog
 PRODUCT_DEVICE := fog
 PRODUCT_MANUFACTURER := Xiaomi
