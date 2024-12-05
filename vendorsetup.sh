2d606d7c5c39f5aebea16a6953f904db2426ff42
alternoegraha <noegrahachan@gmail.com>
2024-12-05 12:07:20 +0000

    fog: @PixelOS-AOSP time
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,8 +7,8 @@ git clone -b refresh https://github.com/alternoegraha/vendor_xiaomi_fog vendor/x
 # Clone kernel tree
 git clone --depth=1 -b makeine-mk2 https://github.com/alternoegraha/wwy_kernel_xiaomi_fog_rebase kernel/xiaomi/fog
 
-# Clone LineageOS' hardware/xiaomi
-git clone -b lineage-22.1 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
+# Clone PixelOS hardware/xiaomi
+git clone -b fifteen https://github.com/PixelOS-AOSP/hardware_xiaomi hardware/xiaomi
 
 # Kernel fixups
 cd kernel/xiaomi/fog
