dd75f0f0581423ff9db356cb91c9ba21e858e015
alternoegraha <noegrahachan@gmail.com>
2025-02-11 23:49:20 +0000

    Revert "fog: drop Face Unlock and Quick Tap definitions"
    This reverts commit 167f8b40a87559a034ab41a43f0bfbcdd11eee31.

--- aosp_fog.mk
+++ aosp_fog.mk
@@ -11,6 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 720
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := false
 
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_INCLUDE_STOCK_ARCORE := false
