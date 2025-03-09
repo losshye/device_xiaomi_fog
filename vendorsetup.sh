23a5424a6f70ced20210fca1fba7c1831290428f
Dwyor Tmx <akunfreefire15417@gmail.com>
2025-03-09 22:56:10 +0000

    fog: @VoltageOs time

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,4 +8,8 @@ git clone -b refresh https://github.com/alternoegraha/vendor_xiaomi_fog vendor/x
 git clone https://github.com/alternoegraha/device_xiaomi_fog-kernel device/xiaomi/fog-kernel
 
 # Clone PixelOS hardware/xiaomi
-git clone -b fifteen https://github.com/PixelOS-AOSP/hardware_xiaomi hardware/xiaomi
+git clone -b fifteen https://github.com/VoltageOs/hardware_xiaomi hardware/xiaomi
+
+# Sign-Key
+rm -rf vendor/lineage-priv
+git clone https://github.com/ardia-kun/vendor -b 14-backup vlp && cp -R vlp/* vendor/ && rm -rf vlp
